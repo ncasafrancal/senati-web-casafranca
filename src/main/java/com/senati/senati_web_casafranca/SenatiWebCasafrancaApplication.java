35c1498e67c4b752f966e169d58be0f2685398d7
Niel Casafranca <ncasafranca@senati.pe>
2025-10-09 09:52:28 -0500

    first commit

---
+++ src/main/java/com/senati/senati_web_casafranca/SenatiWebCasafrancaApplication.java
@@ -0,0 +1,13 @@
+package com.senati.senati_web_casafranca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenatiWebCasafrancaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenatiWebCasafrancaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senati/senati_web_casafranca/SenatiWebCasafrancaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senati.senati_web_casafranca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenatiWebCasafrancaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
