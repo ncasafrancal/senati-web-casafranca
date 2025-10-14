4dc6f50f575b81a916c0d42c6a93f88495a804b5
Niel Casafranca <ncasafranca@senati.pe>
2025-10-14 09:19:43 -0500

    add process new user

--- src/main/java/com/senati/senati_web_casafranca/controller/UserController.java
+++ src/main/java/com/senati/senati_web_casafranca/controller/UserController.java
@@ -3,7 +3,10 @@ package com.senati.senati_web_casafranca.controller;
 import com.senati.senati_web_casafranca.model.User;
 import com.senati.senati_web_casafranca.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,5 +19,10 @@ public class UserController {
     @GetMapping("/api/users")
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @PostMapping("/api/user")
+    public ResponseEntity<User> newUser(@RequestBody User user){
+        return userService.newUser(user);
     }
 }
--- src/main/java/com/senati/senati_web_casafranca/service/UserService.java
+++ src/main/java/com/senati/senati_web_casafranca/service/UserService.java
@@ -3,6 +3,8 @@ package com.senati.senati_web_casafranca.service;
 import com.senati.senati_web_casafranca.model.User;
 import com.senati.senati_web_casafranca.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,5 +16,9 @@ public class UserService {
 
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    public ResponseEntity<User> newUser(User user) {
+        return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
     }
 }
